fcdf3dd77f0c73c93d5b93f7e810879cb1110771
Felipe <giraldofelipe17@gmail.com>
2025-06-21 17:51:26 -0500

    v1.0.0 - Estructura inicial con H2

---
+++ src/main/java/com/entrenapp/backend/EntrenappApplication.java
@@ -0,0 +1,13 @@
+package com.entrenapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrenappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entrenapp/backend/EntrenappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrenapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
